b98c45450f6ef7f9919f1b04e17e4a0461f86498
David Casagrande <dncasagrande@gmail.com>
2016-01-25 11:10:45 -0500

    dont display admin info in rails admin interface

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -40,7 +40,8 @@ RailsAdmin.config do |config|
   config.authenticate_with do
     warden.authenticate! scope: :admin
   end
-  config.current_user_method(&:current_admin)
+
+  config.excluded_models << 'Admin'
 
   config.model 'Band' do
     edit do
