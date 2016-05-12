42a278db8690c60de1d08fe342b92c8fa41c26df
David Casagrande <dncasagrande@gmail.com>
2016-05-12 18:19:39 -0400

    added additional setup for google drive access and added importer

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'rails_admin', '~> 0.8.1'
 gem 'turbolinks', '~> 2.5.3'
 gem 'wombat', '~> 2.5.1'
 gem 'fabrication', '~> 2.14.1'
+gem 'google_drive'
 
 group :production do
   gem 'rails_12factor', '~> 0.0.3'
---
+++ app/interactors/import_google_sheet.rb
@@ -0,0 +1,72 @@
+require 'google_drive'
+
+class ImportGoogleSheet
+  include Interactor
+
+  MAP = {
+    bands: { model: Band, title: 'Herd Fest Bands', serializer: :serialize_band },
+    venues: { model: Venue, title: 'Herd Fest Venues', serializer: :serialize_venue }
+  }
+
+  # context.type
+
+  def call
+    worksheet.rows.each_with_index do |row, idx|
+      next if idx == 0
+
+      data = send(serializer, row)
+
+      unless row[0].empty?
+        model.update(row[0], data)
+      else
+        r = model.create(data)
+        worksheet[idx + 1, 1] = r.id
+      end
+    end
+
+    worksheet.save
+  end
+
+  private
+
+  def session
+    @session ||= GoogleDrive.saved_session('config.json')
+  end
+
+  def type
+    @type ||= MAP[context.type]
+  end
+
+  def title
+    @title ||= type[:title]
+  end
+
+  def serializer
+    @serializer ||= type[:serializer]
+  end
+
+  def model
+    @model ||= type[:model]
+  end
+
+  def worksheet
+    @worksheet ||= session.spreadsheet_by_title(title).worksheets[0]
+  end
+
+  def serialize_band(row)
+    {
+      name: row[1],
+      image_url: row[2],
+      description: row[3],
+      facebook_url: row[4],
+      twitter_url: row[5]
+    }
+  end
+
+  def serialize_venue(row)
+    {
+      name: row[1],
+      street_address: row[2]
+    }
+  end
+end
