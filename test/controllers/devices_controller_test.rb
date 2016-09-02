963724aec045d18ee31e5db43ca7597f70f09273
David Casagrande <dncasagrande@gmail.com>
2016-09-01 22:04:44 -0400

    added devices route with create end point

---
+++ test/controllers/devices_controller_test.rb
@@ -0,0 +1,51 @@
+require 'test_helper'
+
+describe API::DevicesController do
+  describe '#create' do
+    it 'creates a new device if one does not exist with given device_id' do
+      id = SecureRandom.uuid
+      post :create, device_id: id, device_token: 'token'
+
+      assert_response :success
+      assert_equal 1, Device.count
+    end
+
+    it 'does not create a new device if one exists with given device_id' do
+      id = SecureRandom.uuid
+      device = Fabricate(:device, device_id: id)
+
+      post :create, device_id: id, device_token: device.device_token
+
+      assert_response :success
+      assert_equal 1, Device.count
+    end
+
+    it 'does not create a new device if one exists with given device_id and device_token' do
+      id = SecureRandom.uuid
+      device = Fabricate(:device, device_id: id)
+
+      post :create, device_id: id, device_token: 'new-token'
+
+      assert_response :success
+      assert_equal 'new-token', Device.find_by(device_id: id).device_token
+    end
+
+    it 'fails if device_token is sent with nil' do
+      id = SecureRandom.uuid
+      device = Fabricate(:device, device_id: id)
+
+      post :create, device_id: id, device_token: nil
+
+      assert_response 422
+    end
+
+    it 'fails if device_id is sent with nil' do
+      id = SecureRandom.uuid
+      device = Fabricate(:device, device_id: id)
+
+      post :create, device_id: nil, device_token: device.device_token
+
+      assert_response 422
+    end
+  end
+end
