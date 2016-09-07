48581860657be7717a8248a9850830eb25449751
David Casagrande <dncasagrande@gmail.com>
2016-09-07 11:35:02 -0400

    added schedule jsonb column to devices table

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,5 +1,5 @@
 class Device < ActiveRecord::Base
-  crypt_keeper :device_token, encryptor: :postgres_pgp, key: ENV['CRYPT_KEEPER_KEY'], salt: ENV['CRYPT_KEEPER_SALT'], :encoding => 'UTF-8'
+  crypt_keeper :device_token, encryptor: :postgres_pgp, key: ENV['CRYPT_KEEPER_KEY'], salt: ENV['CRYPT_KEEPER_SALT'], encoding: 'UTF-8'
 
   validates :device_id, presence: true
   validates :device_token, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160902064935) do
+ActiveRecord::Schema.define(version: 20160905054604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20160902064935) do
     t.text     "device_token"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.jsonb    "schedule",     default: [], null: false
   end
 
   add_index "devices", ["device_id"], name: "index_devices_on_device_id", using: :btree
