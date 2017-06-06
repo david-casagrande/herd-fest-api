9ffcc3e6b9ac7792672a43dcc3f3fde891c5e33e
David Casagrande <dncasagrande@gmail.com>
2017-06-06 13:28:38 -0400

    filter active set times in venue serializer

--- app/serializers/venue_serializer.rb
+++ app/serializers/venue_serializer.rb
@@ -6,7 +6,7 @@ class VenueSerializer
       id: model.id,
       name: model.name,
       street_address: model.street_address,
-      set_times: model.set_times.map(&:id),
+      set_times: model.set_times.active.map(&:id),
       bands: model.bands.map(&:id)
     }
   end
