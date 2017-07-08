7f9d748365b0a68349913f6daeb42c9f49fce170
David Casagrande <dncasagrande@gmail.com>
2017-07-07 22:58:44 -0400

    band must also be active for set time to be active

--- app/models/set_time.rb
+++ app/models/set_time.rb
@@ -2,7 +2,7 @@ class SetTime < ActiveRecord::Base
   include Cache
 
   default_scope { order(:start_time) }
-  scope :active, -> { joins(:day).where(days: { active: true }) }
+  scope :active, -> { joins(:day, :band).where(days: { active: true }, bands: { active: true }) }
 
   belongs_to :band
   belongs_to :venue
